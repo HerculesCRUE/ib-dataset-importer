0c641955e10b392180d109e5e8fb8991beab3136
IZERTIS\alvaro.blanco <alvaro.blanco@izertis.com>
2021-07-12 19:16:44 +0200

    Fixed worng encoding using CVN endpoint

--- dataset-importer-service/src/main/java/es/um/asio/importer/cvn/service/impl/CvnServiceImpl.java
+++ dataset-importer-service/src/main/java/es/um/asio/importer/cvn/service/impl/CvnServiceImpl.java
@@ -83,7 +83,7 @@ public class CvnServiceImpl implements CvnService {
     @Retryable(value = { CvnRequestException.class }, maxAttempts = 3, backoff = @Backoff(delay = 3000) )
     public CvnRootBean findById(Long id) {
 		String uri = cvnEndpoint.concat(cvnEndpointContext).concat("?id=" + id.toString());
-		restTemplate.getMessageConverters().add(0,  new StringHttpMessageConverter(StandardCharsets.UTF_8));
+		restTemplate.getMessageConverters().add(0,  new StringHttpMessageConverter(StandardCharsets.ISO_8859_1));
         ResponseExtractor<CvnRootBean> cvnResponseExtractor =  new CvnResponseExtractor(restTemplate.getMessageConverters());        
         try {        	
             return restTemplate.execute(uri, HttpMethod.GET, clientHttpRequest -> clientHttpRequest.getHeaders().addAll(getHeaders()), cvnResponseExtractor);
