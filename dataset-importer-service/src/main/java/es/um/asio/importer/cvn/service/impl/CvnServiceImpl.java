1ab277bcefead0d0408d96c158a017834fa9c179
IZERTIS\alvaro.blanco <alvaro.blanco@izertis.com>
2021-07-25 23:11:40 +0200

    [SEMANTMURC-680] Importacion Cerif

--- dataset-importer-service/src/main/java/es/um/asio/importer/cvn/service/impl/CvnServiceImpl.java
+++ dataset-importer-service/src/main/java/es/um/asio/importer/cvn/service/impl/CvnServiceImpl.java
@@ -12,6 +12,7 @@ import javax.xml.bind.Unmarshaller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.http.HttpEntity;
