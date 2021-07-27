9a93a02a2aa205ba676d0fdb2ec8a6dee22c7ff2
IZERTIS\alvaro.blanco <alvaro.blanco@izertis.com>
2021-07-27 19:30:06 +0200

    [SEMANTMURC-680] Added correct version information in cerif data events

--- dataset-importer-service/src/main/java/es/um/asio/importer/cerif/processor/DataCerifProcessor.java
+++ dataset-importer-service/src/main/java/es/um/asio/importer/cerif/processor/DataCerifProcessor.java
@@ -1,10 +1,6 @@
 package es.um.asio.importer.cerif.processor;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.StepContribution;
-import org.springframework.batch.core.StepExecution;
-import org.springframework.batch.core.annotation.BeforeStep;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
@@ -13,25 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import es.um.asio.importer.cerif.service.CerifService;
 
 public class DataCerifProcessor implements Tasklet {
-	/**
-	 * The job execution id.
-	 */
-	private long jobExecutionId;
 
 	@Autowired
 	private CerifService cerifService;
 
-	/**
-	 * @inheritDoc
-	 */
-	@BeforeStep
-	public void beforeStep(StepExecution stepExecution) {
-		jobExecutionId = stepExecution.getJobExecutionId();
-	}
-
 	@Override
-	public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-		cerifService.importCerifData(jobExecutionId);
+	public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {		
+		cerifService.importCerifData(contribution.getStepExecution().getJobExecution().getId());
 		return RepeatStatus.FINISHED;
 	}
 }
