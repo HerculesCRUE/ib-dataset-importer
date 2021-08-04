016d5489202f33e67ac8c2cb1c744d272ad4ba70
Atorrecilla <atorrecilla@izertis.com>
2021-08-04 08:48:58 +0200

    Fix to build

--- dataset-importer-back/src/main/java/es/um/asio/importer/back/controller/ImporterController.java
+++ dataset-importer-back/src/main/java/es/um/asio/importer/back/controller/ImporterController.java
@@ -1,8 +1,6 @@
 package es.um.asio.importer.back.controller;
 
 import java.security.InvalidParameterException;
-import java.util.List;
-import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
@@ -22,7 +20,6 @@ import es.um.asio.domain.importer.ImporterSchedule;
 import es.um.asio.importer.back.dto.DataImporterDTO;
 import es.um.asio.importer.back.dto.ImportExecutionDTO;
 import es.um.asio.importer.back.mapper.ImporterMapper;
-import es.um.asio.importer.dto.ImportExecutionVO;
 import es.um.asio.importer.service.ImporterExecutionService;
 import es.um.asio.importer.service.ImporterSchedulerService;
 import lombok.AccessLevel;
@@ -34,54 +31,54 @@ import lombok.NoArgsConstructor;
 @RestController
 @RequestMapping(ImporterController.Mappings.BASE)
 public class ImporterController {
-	
+
 	@Autowired
 	private ImporterSchedulerService importerSchedulerService;
-	
+
 	@Autowired
 	private ImporterMapper importerMapper;
-	
+
 	@Autowired
 	private ImporterExecutionService importerExecutionService;
-	
+
 	@PostMapping(value = ImporterController.Mappings.SCHEDULE)
 	public ResponseEntity<Void> schedule(@RequestBody @Valid DataImporterDTO dataImporterDTO) {
 		ImporterSchedule importerSchedule = importerMapper.mapDataImporterDTOToImporterSchedule(dataImporterDTO);
 		importerSchedulerService.createSchedule(importerSchedule);
 		return new ResponseEntity<Void>(HttpStatus.OK);
 	}
-	
+
 	@GetMapping(value = ImporterController.Mappings.SEARCH)
-	public Page<ImportExecutionDTO> search(final Pageable pageable) {		
+	public Page<ImportExecutionDTO> search(final Pageable pageable) {
 		Page<ImportExecutionDTO> result = importerExecutionService.findImporterExecutions(pageable)
 				.map(importerMapper::mapImportExecutionVOToImportExecutionDTO);
-		
+
 		return result;
 	}
-	
+
 	@ExceptionHandler(InvalidParameterException.class)
 	public ResponseEntity<Void> handleInvalidParameterException() {
-		 return new ResponseEntity<Void>(HttpStatus.UNPROCESSABLE_ENTITY);
+		return new ResponseEntity<Void>(HttpStatus.UNPROCESSABLE_ENTITY);
 	}
-		
+
 	/**
-     * Mappgins.
-     */
-    @NoArgsConstructor(access = AccessLevel.PRIVATE)
-    static final class Mappings {
-        /**
-         * Controller request mapping.
-         */
-        protected static final String BASE = "/importer";
-        
-        /**
-         * Mapping for schedule.
-         */
-        protected static final String SCHEDULE = "/schedule";
-        
-        /**
-         * Mapping for search.
-         */
-        protected static final String SEARCH = "/search";
-    }
+	 * Mappgins.
+	 */
+	@NoArgsConstructor(access = AccessLevel.PRIVATE)
+	static final class Mappings {
+		/**
+		 * Controller request mapping.
+		 */
+		protected static final String BASE = "/importer";
+
+		/**
+		 * Mapping for schedule.
+		 */
+		protected static final String SCHEDULE = "/schedule";
+
+		/**
+		 * Mapping for search.
+		 */
+		protected static final String SEARCH = "/search";
+	}
 }
